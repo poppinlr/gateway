89cc876f8ee63f5027485e9cd6b14defb7d9f54c
zhuochen_shen <szc186!?szc>
2018-09-17 16:34:47 +0800

    route config added

--- src/main/java/com/zhuochen/gateway/GatewayApplication.java
+++ src/main/java/com/zhuochen/gateway/GatewayApplication.java
@@ -2,7 +2,10 @@ package com.zhuochen.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 
 /**
@@ -13,6 +16,16 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("um_route", r -> r
+                        .path("/clinetc/**")
+                        .uri("lb://clinet/")
+                )
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
